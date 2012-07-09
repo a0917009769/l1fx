cb0a1c5a8021394342800a471c2424cab1c51f59
Men <firebom@live.com>
2012-07-09 15:45:08 +0800

    init

---
+++ src/com/l1fx/Start.java
@@ -0,0 +1,13 @@
+package com.l1fx;
+
+public class Start {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("i will do something...");
+	}
+
+}
